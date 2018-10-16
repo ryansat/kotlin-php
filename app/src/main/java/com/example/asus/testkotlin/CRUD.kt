ec8ca863ebacbb3bcb64921507916a6a9d967e9b
Fajar Satria <fajar@andromedia.co.id>
2018-10-16 18:00:28 +0700

    add view json from php
    add view json from php

--- app/src/main/java/com/example/asus/testkotlin/CRUD.kt
+++ app/src/main/java/com/example/asus/testkotlin/CRUD.kt
@@ -3,9 +3,11 @@ package com.example.asus.testkotlin
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_crud.*
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.entity.StringEntity
@@ -15,12 +17,16 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.util.concurrent.TimeUnit
 
 class CRUD : AppCompatActivity() {
 
      var id : String = ""
      var name : String = ""
      var hasil : String = ""
+     var jsondata : String = ""
+     var data : String = ""
+     var jObject = JSONObject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crud)
@@ -46,6 +52,32 @@ class CRUD : AppCompatActivity() {
         id =  editText.text.toString()
         name = editText2.text.toString()
         HttpAsyncTaskPost().execute("http://10.0.2.2:8070/maps/update.php?id=$id&name=$name")
+    }
+
+    public fun btnView(v : View)
+    {
+        id =  editText.text.toString()
+        name = editText2.text.toString()
+
+        try{
+        data = ""
+        jsondata = ""
+        HttpAsyncTaskPosts().execute("http://10.0.2.2:8070/maps/tampil.php")
+        Thread.sleep(1000)
+        // Show all data
+        parseBoundary()
+        showMessage("Data", data)}
+        catch (ex :Exception){
+            Toast.makeText(getBaseContext(), "failed get data", Toast.LENGTH_LONG).show()
+        }
+3    }
+
+    fun showMessage(title: String, Message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setCancelable(true)
+        builder.setTitle(title)
+        builder.setMessage(Message)
+        builder.show()
     }
 
     fun POST(url: String, id: String, name : String): String {
@@ -111,6 +143,70 @@ class CRUD : AppCompatActivity() {
         return result
     }
 
+    fun GET(url: String, id: String, name : String): String {
+        var inputStream: InputStream? = null
+        var result = ""
+        try {
+            jsondata = ""
+
+            // 1. create HttpClient
+            val httpclient = DefaultHttpClient()
+
+            // 2. make POST request to the given URL
+            val httpGet = HttpGet(url)
+
+            var json = ""
+
+            // 3. build jsonObject
+            val jsonObject = JSONObject()
+            jsonObject.accumulate("username", id)
+            jsonObject.accumulate("name", name)
+            //jsonObject.accumulate("latitude", loc.getLatitude())
+            // jsonObject.accumulate("updatestatus", loc.getUpdate())
+
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString()
+
+            // 5. set json to StringEntity
+            val se = StringEntity(json)
+
+            // 6. set httpPost Entity
+            //httpGet.sesetEntity(se)
+
+            // 7. Set some headers to inform server about the type of the content
+            httpGet.setHeader("Accept", "application/json")
+            httpGet.setHeader("Content-type", "application/json")
+
+            // 8. Execute POST request to the given URL
+            val httpResponse = httpclient.execute(httpGet)
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent()
+
+            // 10. convert inputstream to string
+            if (inputStream != null)
+            {
+                //result = inputStream.bufferedReader().use(BufferedReader::readText)
+                result = convertInputStreamToString(inputStream)
+                //Toast.makeText(getBaseContext(), ""+result, Toast.LENGTH_LONG).show()
+            }
+
+            else
+            {
+                result = "Did not work!"
+            }
+
+            jsondata += result
+
+        } catch (e: Exception) {
+            Log.d("InputStream", e.localizedMessage)
+        }
+
+        // 11. return result
+        return jsondata
+    }
+
     @Throws(IOException::class)
     private fun convertInputStreamToString(inputStream: InputStream): String {
 
@@ -130,9 +226,39 @@ class CRUD : AppCompatActivity() {
 
 
 
+
+    private fun parseBoundary() {
+        var xResultBoundary : String =""
+        //xResultBoundary = getRequestBoundary("http://10.0.2.2:8070/maps/tampil.php")
+        jObject = JSONObject(jsondata)
+        val menuitemArray = jObject.getJSONArray("user")
+        //var sret = ""
+        for (i in 0 until menuitemArray.length()) {
+
+            data += menuitemArray.getJSONObject(i).getString("id").toString() + " : "
+            data += menuitemArray.getJSONObject(i).getString("name").toString() + "\n"
+
+        }
+        //txtResult.setText(sret);
+    }
+
+
+
     private inner class HttpAsyncTaskPost : AsyncTask<String, Void, String>() {
         override fun doInBackground(vararg urls: String): String {
             return POST(urls[0], id,name)
+        }
+
+        override fun onPostExecute(result: String) {
+            //Toast.makeText(applicationContext, "User : $userid, Lat : $lat, Long : $lng", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(getBaseContext(), ""+hasil, Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private inner class HttpAsyncTaskPosts : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg urls: String): String {
+            return GET(urls[0], id,name)
         }
 
         override fun onPostExecute(result: String) {
